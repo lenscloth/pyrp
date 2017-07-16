503a0f72aeb94c3be24f0c11a36e0948a999f512
Wonpyo Park <wppark.pio@gmail.com>
2017-07-16 16:00:53 +0900

    Feat: config.py and README

--- README.md
+++ README.md
@@ -1,35 +1,26 @@
-Introduction
-============
+# Introduction
+
 Randomized Prim Algorithm for Object Proposals on Python3
 Forked form https://github.com/prlz77/pyrp
 
-pyrp
-====
-
+# pyrp
 Ctypes Python wrapper for S. Manen, M. Guillaumin, L. Van Gool's Random Prim's Algorithm for Object Proposals.
-
 Visit: https://github.com/smanenfr/rp to get the original code.
 
-Set-Up
-======
-You can change configuration by changing the value at config.py
+# Quick Start
 
-
-
+#### 1. Change config.py and Save configruation
+Change configuration at config.py or you can use default configuration.
+Check https://github.com/smanenfr/rp to know the details about configuration
+Save configuration
+```
+python3 config.py #rp.npy is the default saved configuration filename
+```
+### 2. compile
 ```
 make
 ```
-
-Hints
-=====
-
-rp.mat parameters translated to rp.npy
-
-
-Demo
-====
-
-To check it working execute python demo.py inside the pyrp folder.
-
-Usage:
-	python demo.py [image=image_path] [savefile=(save_file.npy | save_file.mat)]
+### 3. Run demo
+```
+python3 demo.py [image=image_path] [savefile=(save_file.npy | save_file.mat)]
+```
---
+++ config.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+params = {}
+# Parameter specification:
+params['approxFinalNBoxes'] = 10000  # Approximate number of proposals
+params['rSeedForRun'] = -1 # Random seed to be used (-1 to generate it with a hashing function)
+params['q'] = 10 # Parameter to eliminate near duplicates (raise it to eliminate more duplicates)
+
+# LAB segmentation
+params['colorspace'] = 'LAB'  # Colorspace: 'RGB', 'LAB', 'opponent', 'rg', 'HSV'
+
+# superpixels
+params['superpixels'] = {}
+params['superpixels']['sigma'] = 0.8
+params['superpixels']['c'] = 100
+params['superpixels']['min_size'] = 100
+
+# Parameters trained from VOC07:
+params['simWeights'] = {}
+params['simWeights']['wLABColorHist'] = -2.6864
+params['simWeights']['wBias'] = 3.0017
+params['simWeights']['wCommonBorder'] =  -1.0029
+params['simWeights']['wSizePer'] = -2.3655
+
+# Size term alpha, as explained in the paper sec. 4.2.
+# It is quantized to contain exactly 2^16 elements for speed
+
+# The paper:
+# S. Manen, M. Guillaumin, and L. Van Gool.
+# Prime Object Proposals with Randomized Prim's Algorithm. In ICCV, 2013.
+
+params['alpha'] = np.load('./data/alpha_voc07.npy');
+params['verbose'] = False; # Set to true to display more information during execution
+
+if __name__ == "__main__":
+    np.save('./rp.npy', params)
+    
